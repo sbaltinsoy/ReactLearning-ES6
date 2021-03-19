d039578bd763ec1111ab389e1b8396c5b7b124db
sbaltinsoy <serhatburakaltinsoy@gmail.com>
2021-03-19 10:19:18 +0300

    match objesi router parametreleri

--- ReactProjeler/react_temelleri/src/components/About.jsx
+++ ReactProjeler/react_temelleri/src/components/About.jsx
@@ -1,3 +1,26 @@
 import React from "react"
 
 export const About = () => <div style={{textAlign: "center"}}>About</div>
+
+/*
+class About extends React.Component {
+  constructor(props){
+    super(props);
+    console.log(props);
+  }
+
+  changeUrlToHell = () => {
+    this.props.history.push("/hell")
+  }
+
+  render(){
+    return(
+      <div>
+        About
+        <button onClick = {this.changeUrlToHell}>Redirect To Hell</button>
+      </div>
+    );
+
+  }
+}
+*/
